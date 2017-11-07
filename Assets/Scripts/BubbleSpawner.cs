f5d9dd57efbaab0a0d566db1c6599303564ab048
Ishan Mishra <mishrai@uwindsor.ca>
2017-11-06 23:38:27 -0500

    added the game

---
+++ Assets/Scripts/BubbleSpawner.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleSpawner : MonoBehaviour {
+
+	float min = -600f;
+	float max = 600f;
+	List<GameObject> spheres;
+	float timer;
+	TextMesh text;
+	float nextActionTime = 0f;
+	float period = 0.05f;
+	int popped = 0;
+	float spawnRate = 4f;
+	float r;
+	// Use this for initialization
+	void Start () {
+		spheres = new List<GameObject> ();
+		timer = 0.0f;
+		text = GameObject.Find ("Text").GetComponent<TextMesh> ();
+		InvokeRepeating ("spawnBubble", 0.0f, spawnRate);
+	}
+
+	Vector3 getRandomPosition(){
+		r = Random.Range (0f, 1f);
+		if (r <= 0.25) {
+			return new Vector3 (-9f, 0, 0);
+		} else if (r <= 0.5) {
+			return new Vector3 (9f, 0, 0);
+		} else if (r <= 0.75) {
+			return new Vector3 (0, 0, -9f);
+		} else {
+			return new Vector3 (0, 0, 9f);
+		}
+	}
+
+	float rand(float min, float max){
+		return Random.Range (min, max);
+	}
+
+	void addAccordingForce(GameObject sphere){
+		Vector3 forceVector;
+		if (r <= 0.25) {
+			forceVector = new Vector3 (rand (50, max), 0, 0);
+		} else if (r <= 0.5) {
+			forceVector = new Vector3 (-(rand (50, max)), 0, 0);
+		} else if (r <= 0.75) {
+			forceVector = new Vector3 (0, 0, rand (50, max));
+		} else {
+			forceVector = new Vector3 (0, 0, -(rand (50, max)));
+		}
+		sphere.GetComponent<Rigidbody>().AddForce(transform.position + forceVector);
+	}
+
+	void spawnBubble(){
+		GameObject sphere = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+		sphere.transform.position = getRandomPosition ();
+		sphere.AddComponent<Rigidbody> ();
+		sphere.GetComponent<Rigidbody> ().useGravity = false;
+		sphere.GetComponent<Rigidbody> ().detectCollisions = false;
+		addAccordingForce (sphere);
+		sphere.AddComponent(System.Type.GetType("Controller"));
+		spheres.Add (sphere);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		int seconds = (int) timer % 60;
+		text.text = "Timer: " + seconds + "s";
+
+		if (Controller.popped > this.popped) {
+			this.popped++;
+			CancelInvoke ();
+			spawnRate = spawnRate / 1.75f;
+			InvokeRepeating ("spawnBubble", 0.0f, spawnRate);
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	static TextMesh poppedText;
+	static TextMesh missedText;
+	public static int popped = 0;
+	static int missed = 0;
+	bool onplane = false;
+	GameObject plane;
+	Vector3 center;
+	Material mat;
+	float time = 10;
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Renderer> ().material = Resources.Load("Blue", typeof(Material)) as Material;
+		gameObject.GetComponent<Collider> ().material = (PhysicMaterial)Resources.Load ("Physic", typeof(PhysicMaterial)) as PhysicMaterial;
+		gameObject.GetComponent<Rigidbody> ().drag = 0f;
+		gameObject.GetComponent<Rigidbody> ().angularDrag = 0f;
+		if (poppedText == null || missedText == null) {
+			poppedText = GameObject.Find ("Popped").GetComponent<TextMesh> ();
+			missedText = GameObject.Find ("Missed").GetComponent<TextMesh> ();
+			poppedText.text += "0";
+			missedText.text += "0";
+		}
+	}
+
+	void OnCollisionEnter(){
+
+	}
+
+	bool inBounds(){
+		if (gameObject.transform.position.x < 5f && gameObject.transform.position.x > -5f) {
+			return true;
+		}
+		return false;
+	}
+
+	void OnMouseDown(){
+		Destroy (gameObject);
+		popped++;
+		poppedText.text = "Bubbles popped: " + popped;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!onplane && inBounds()) {
+			gameObject.GetComponent<Rigidbody> ().detectCollisions = true;
+			onplane = true;
+		}
+		time -= Time.deltaTime;
+		if (time < 0) {
+			Destroy (gameObject);
+			missed++;
+			missedText.text = "Bubbles missed: " + missed;
+		}
+
+		if (transform.position.y > 1f)
+			transform.position = new Vector3 (transform.position.x, 0, transform.position.z);
+
+		if (missed > 10)
+			Application.Quit ();
+	}
+}
